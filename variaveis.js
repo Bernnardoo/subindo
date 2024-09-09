7782c1996855a555a4a016add5f1a55798031e07
Bernnardoo <bernnardoolivjobs@icloud.com>
2024-09-09 16:51:15 -0300

    subindo

---
+++ variaveis.js
@@ -0,0 +1,21 @@
+// string
+var nome="bernnardo"
+
+// number
+var numeroInteiro = -7
+
+// number  float
+var numeroFracionado = 9.5 /*sempre fracionamos com um .  */
+
+
+// boleano
+var boleano = true // pode ser true e false 
+
+
+
+alert(nome) // abre um dialog  ,com uma mensagem para o usuário
+
+
+document.write(numeroInteiro)
+
+console.log(numeroInteiro)
